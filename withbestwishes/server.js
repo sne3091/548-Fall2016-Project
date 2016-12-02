5d3bf46833a989a584dac4cfd1b7e9122e46d51e
Sneha Srinivasan <snehasri@usc.edu>
2016-12-02 14:43:12 -0800

    Code added for gift retrieval from database

--- withbestwishes/server.js
+++ withbestwishes/server.js
@@ -27,39 +27,71 @@
     }
     var Twitter = new TwitterPackage(secret);
 
-        // define model =================
-        var Tweet = mongoose.model('Tweet', {
-        	text : String
-        });
+    var schema = new mongoose.Schema({ _id: mongoose.Schema.Types.ObjectId, url: String, title: String, rating: String, price_min: Number, price_max: Number,
+    image: String, category: String });
+    // define model =================
+    var Gifts = mongoose.model('Gifts', schema);
     // routes ======================================================================
 
     // api ---------------------------------------------------------------------
     // get all todos
-    app.post('/api/tweets', function(req, res) { 
+    var domain_nba = ['LA Lakers','New York Knicks','Chicago Bulls','Golden State Warriors','San Antonio Spurs','Michael Jordan',
+    'Stephen Curry','Kobe Bryant','Joakim Noah','Tim Duncan'];
+    var domain_nfl = ['New England Patriots','Seattle Seahawks','Carolina Panthers','Dallas Cowboys','Denver Broncos','Tom Brady',
+    'Cam Newton','Russell Wilson','Dak Prescott','Peyton Manning'];
+    var domain_soccer = ["Manchester United","FC Barcelona","Arsenal","Real Madrid","Chelsea","Wayne Rooney","Lionel Messi","Mesut Ozil",
+    "Cristiano Ronaldo","Neymar"];
+    var domain_ncaa = ['USC Trojans',"Ohio State Buckeyes","Alabama Football","Washington Athletics","UCLA Athletics"];
+    var domain_bb = ["Chicago Cubs","Los Angeles Dodgers","New York Yankees","San Francisco Giants","Boston Red Sox","Kris Bryant","Clayton Kershaw",
+    "Gary Sanchez","Dustin Pedroia","Madison Bumgarner"];
+    var domain_hockey = ["Columbus Blue Jackets","Pittsburgh Penguins","New York Rangers","Philadelphia Flyers","Carolina Hurricanes"];
+    var domain_tennis = ["Rafael Nadal","Roger Federer","Serena Williams","Maria Sharapova","Novak Djokovic"];
 
+    function getGiftsQuery(category, price_min, price_max){
+        var query = Gifts.find({category:{ $in:category }, price_min:{ $gte: price_min }, price_max: { $lte: price_max }});
+        return query;
+    }
+
+    app.post('/api/getItems', function(req, res) { 
+            var search_by = req.body.searchby;
     		var options = { screen_name: req.body.text, count: 200 };
-
-    		Twitter.get('friends/list', options , function(err, data) {
-    			res.json(data);
-    		});
+            var budget = req.body.budget;
+            var price_arr = budget.split("-");
+            var price_min=parseInt(price_arr[0].substring(1));
+            var price_max=parseInt(price_arr[1].substring(1));
+            if(search_by=='twitter'){
+        		Twitter.get('friends/list', options , function(err, data) {
+                    var interest = [];
+                    for (var i = data.users.length - 1; i >= 0; i--) {
+                        if(domain_nba.indexOf(data.users[i].name) > -1 || domain_nfl.indexOf(data.users[i].name) > -1 ||
+                         domain_soccer.indexOf(data.users[i].name) > -1 || domain_ncaa.indexOf(data.users[i].name) > -1 || 
+                         domain_bb.indexOf(data.users[i].name) > -1 || domain_hockey.indexOf(data.users[i].name) > -1 || 
+                         domain_tennis.indexOf(data.users[i].name) > -1 ){
+                            interest.push(data.users[i].name);    
+                        }
+                    }
+                    var query = getGiftsQuery(interest, price_min, price_max);
+                    query.exec(function(err,items){
+                        if(err)
+                          return console.log(err);
+                        res.json(items);
+                    });
+        		});
+            }else{
+                var input = req.body.text.split(",");
+                var interest = []
+                for (var i = input.length - 1; i >= 0; i--){
+                    interest.push(input[i]); 
+                }
+                var query = getGiftsQuery(interest, price_min, price_max);
+                query.exec(function(err,items){
+                    if(err)
+                      return console.log(err);
+                    res.json(items);
+                });
+            }
     	});
 
-    // delete a todo
-    app.delete('/api/tweet/:tweet_id', function(req, res) {
-    	Tweet.remove({
-    		_id : req.params.tweet_id
-    	}, function(err, todo) {
-    		if (err)
-    			res.send(err);
-
-            // get and return all the todos after you create another
-            Tweet.find(function(err, todos) {
-            	if (err)
-            		res.send(err)
-            	res.json(todos);
-            });
-        });
-    });
 
     // application -------------------------------------------------------------
     app.get('*', function(req, res) {
