410e3911b421c4caa244d53f7ffe6dfaafb41e2b
Sneha Srinivasan <snehasri@usc.edu>
2016-12-03 14:50:40 -0800

    Dynamically adding items

--- withbestwishes/public/core.js
+++ withbestwishes/public/core.js
@@ -1,17 +1,17 @@
 
-angular.module('withBestWishes', [])
+angular.module('withBestWishes', ['ui.bootstrap'])
 .controller('mainController', ['$scope','$http', function mainController($scope,$http) {
     $scope.formData = {};
     $scope.resultsVisible = false;
      // when submitting the add form, send the text to the node API
      $scope.fetchResults = function() {
         $http.post('/api/getItems', $scope.formData)
-            .success(function(data) {
-                $scope.resultsVisible =  $scope.resultsVisible ? false : true;
-                $scope.items = data;
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
+        .success(function(data) {
+            $scope.resultsVisible = true;
+            $scope.currentItems = data;
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
     };
 }]);
